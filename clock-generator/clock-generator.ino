1cf5112392fa9c76a7c38fce869bb5dfcbe38293
bberak <bberak@gmail.com>
2020-11-09 02:16:34 +1100

    Made the maz hz a bit easier to control and logic easier to follow with variables that can be modified at the top of the sketch

--- clock-generator/clock-generator.ino
+++ clock-generator/clock-generator.ino
@@ -1,4 +1,7 @@
 bool signal = 0;
+int maxHz = 100;
+int maxPeriod = 1000;
+int minPeriod = maxPeriod / maxHz;
 
 void setup()
 {
@@ -11,18 +14,15 @@ void loop()
   //-- Toggle signal
   signal = !signal;
 
-  //-- Read analog input, and round into 100 bands.
-  //-- Then multiply band by 10 to give a period between
-  //-- 0ms and 1000ms (effectively this generates a max
-  //-- clock cycle of 100hz).
+  //-- Read analog input and scale to maxHz
   int raw = analogRead(A6);
-  int period = map(raw, 0, 1023, 1, 100) * 10;
+  int period = map(raw, 0, 1023, 1, maxHz) * minPeriod;
   
-  //-- Only output high if the signal is on and the period is less than 1000.
-  //-- A period of 1000ms indicates the slowest speed setting (potentiometer is
+  //-- Only output high if the signal is on and the period is less than maxPeriod.
+  //-- A maxPeriod indicates the slowest speed setting (potentiometer is
   //-- wound all the way anti-clockwise) and it's at this point that the user
   //-- can perform manualy clock stepping using the physical switch. 
-  digitalWrite(9, signal && period < 1000 ? 1 : 0);
+  digitalWrite(9, signal && period < maxPeriod ? 1 : 0);
 
   //-- Sleep based on the selected period (in ms)
   delay(period);
